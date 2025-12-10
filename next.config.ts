e34c697a6177788ef4f06fc025ad956fe05345ed
Julian Barabino <julianbarabino@gmail.com>
2025-12-10 21:28:51 +0000

    Ready for Launch: Ignorando warning de IDX

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,8 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {};
-export default nextConfig;
 
+const nextConfig: NextConfig = {
+  /* config options here */
+  // Dejamos esto limpio para evitar errores de versión
+};
+
+export default nextConfig;
